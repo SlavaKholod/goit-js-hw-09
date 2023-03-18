20b949e2356a37af22b7fab2cc5d8cfe2d2f2baf
SlavaKholod <kholod.v.job@gmail.com>
2023-03-18 20:08:34 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const refs = {
+  body: document.body,
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+// console.log(refs);
+let currentColor = '#ffffff';
+let timerId = null;
+refs.stop.disabled = true;
 
+refs.start.addEventListener('click', () => {
+  refs.start.disabled = true;
+  if (timerId !== null) {
+    return;
+  } else {
+    timerId = setInterval(() => {
+      currentColor = getRandomHexColor();
+      addColor();
+      console.log(currentColor);
+    }, 1000);
+    refs.stop.disabled = false;
+  }
+});
+
+refs.stop.addEventListener('click', onStopChangeColor);
+
+function addColor() {
+  refs.body.style.backgroundColor = currentColor;
+}
+
+function onStopChangeColor() {
+  clearInterval(timerId);
+  timerId = null;
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
